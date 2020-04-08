8894e5be5a9545ffaa1e08e224a37baa56e64008
voltamage <37029882+voltamage@users.noreply.github.com>
2020-04-08 04:55:25 -0700

    Update aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,6 +2,7 @@ alias backport="dpkg -l | grep '~bpo'"
 alias crsync='sudo rsync --acls --archive --block-size=131072 --partial --progress --recursive --stats -vv'
 alias diskhealth='sudo smartctl -i /dev/sda && sudo smartctl -A /dev/sda'
 alias glusterc='sudo ls -la -R /mnt/gluster | wc -l'
+alias glusterv="sudo glusterfs --version | grep 'glusterfs '"
 alias hostsupdate='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/armbian-glusterfs/master/tmp/hosts_updater)"'
 alias iplist='nmap -sP 192.168.2.1/24'
 alias mntdu='sudo ncdu /mnt'
